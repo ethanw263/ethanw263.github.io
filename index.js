e8e28ca36deb6197f54443d8b875e9d1d6a1f25f
Ethan <ethanw263@gmail.com>
2024-04-23 15:18:55 -0700

    css js assets added

---
+++ index.js
@@ -0,0 +1,63 @@
+// template_jlbj2il 
+// service_80i6qos
+// HtznVROl2fjdvyzwd
+
+
+let isModalOpen = false;
+let contrastToggle = false;
+const scaleFactor = 1/20;
+
+function moveBackground(event) {
+    const shapes = document.querySelectorAll(".shape")
+    const x = event.clientX * scaleFactor
+    const y = event.clientY * scaleFactor
+
+    for (let i = 0; i < shapes.length; ++i) {
+        const isOdd  = i % 2 !==0
+        const boolInt = isOdd ? -1 : 1
+        shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px)`
+    }
+}
+
+function toggleContrast() {
+    contrastToggle = !contrastToggle
+    if (contrastToggle) {
+        document.body.classList += " dark-theme"
+    } else {
+        document.body.classList.remove('dark-theme')
+    }
+}
+
+
+function contact(event) {
+    event.preventDefault()
+    const loading = document.querySelector('.modal__overlay--loading')
+    const success = document.querySelector('.modal__overlay--success')
+    emailjs
+        .sendForm(
+            'service_80i6qos', 
+            'template_jlbj2il', 
+            event.target, 
+            'HtznVROl2fjdvyzwd'
+        ).then(() => {
+            loading.classList.remove("modal__overlay--visible")
+            success.classList += " modal__overlay--visible"
+            
+        }).catch(() => {
+            loading.classList.remove("modal__overlay--visible")
+            alert(
+                "The email service is temporarily unavailable. Please contact me directly at ethanw263@gmail.com"
+            )
+    })
+}
+
+function toggleModal() {
+    // toggle modal
+
+    if (isModalOpen) {
+        isModalOpen = false
+        return document.body.classList.remove("modal--open")
+    }
+    isModalOpen = true;
+    document.body.classList += " modal--open"
+}
